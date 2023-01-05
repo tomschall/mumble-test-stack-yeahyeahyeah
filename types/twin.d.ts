256c37974fc7cd71cb70916e406710cff08afc90
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-01-05 17:03:05 +0100

    configure project for using tailwind, twin macro and custom npm package

---
+++ types/twin.d.ts
@@ -0,0 +1,34 @@
+import "twin.macro"
+import styledImport, { CSSProp, css as cssImport } from "styled-components"
+
+declare module "twin.macro" {
+	// The styled and css imports
+	const styled: typeof styledImport
+	const css: typeof cssImport
+}
+
+declare module "react" {
+	// The css prop
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	interface HTMLAttributes<T> extends DOMAttributes<T> {
+		css?: CSSProp
+		tw?: string
+	}
+	// The inline svg css prop
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+		css?: CSSProp
+		tw?: string
+	}
+}
+
+// The 'as' prop on styled components
+declare global {
+	namespace JSX {
+		interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+			as?: string
+		}
+	}
+
+	type TSpacing = "0" | "2" | "4" | "8" | "16" | "32" | "64"
+}
