bbec4d9c05e07664a63bbe5303d90994ab5602fa
webrooster <roland.vonaesch@fhnw.ch>
2023-03-01 12:51:27 +0100

    test husky

--- services/fetchMumbles.ts
+++ services/fetchMumbles.ts
@@ -3,6 +3,8 @@ import { transformMumble, QwackerMumbleResponse } from './qwacker';
 export const fetchMumbles = async (params?: { limit?: number; offset?: number; newerThanMumbleId?: string }) => {
   const { limit, offset, newerThanMumbleId } = params || {};
 
+  console.log(limit);
+
   const url = `${process.env.NEXT_PUBLIC_QWACKER_API_URL}/posts?${new URLSearchParams({
     limit: limit?.toString() || '10',
     offset: offset?.toString() || '0',
